61086c7bd6be8c17af441ecc6fc4f01da92eee46
lidongze0629 <9546726@qq.com>
2018-10-13 00:11:15 +0800

    add bubble sort

---
+++ interview/sort/BubbleSort.cpp
@@ -0,0 +1,105 @@
+#include <iostream>
+#include <vector>
+
+template <typename T>
+using Vector = std::vector<T>;
+
+// 第一种方式，每一次排序使得最小的数位于第一个
+// 此时外层循环 i 就是每趟排序要确定的最小值。
+// 算法的复杂度是n^2, 并且没有优化的地步。
+// (n-1)(n-2) / 2
+size_t BubbleSort_min(Vector<int>& vec) {
+    size_t times = 0;
+    for (size_t i = 0; i < vec.size() - 1; i++) {
+        for (size_t j = i + 1; j < vec.size(); j++) {
+            times++;
+            if (vec[i] > vec[j]) {
+                std::swap(vec[i], vec[j]);
+            }
+        }
+    }
+    return times;
+}
+
+// 第二种方式，每一次排序使得最大的数位于最后一个
+// 此时外层循环 i 通过内存for循环的 j < vec.size() - i - 1
+// 控制着每趟排序最大值
+// 算法的复杂度也是n^2，但是有优化的地步
+// 通过内部加一个flag可以大幅度缩减循环次数。
+size_t BubbleSort_max(Vector<int>& vec) {
+    size_t times = 0;
+    bool flag = true;
+    for (size_t i = 0; i < vec.size() - 1 && flag; i++) {
+        for (size_t j = 0; j < vec.size() - 1 - i; j++) {
+            times++;
+            if (vec[j] > vec[j + 1]) {
+                flag = false;
+                std::swap(vec[j], vec[j+1]);
+            }
+        }
+    }
+    return times;
+}
+
+void TestMin() {
+    Vector<int> input;
+    for (unsigned i = 1; i < 11; ++i) {
+        input.push_back(i);
+    }
+
+    for (unsigned i = 15; i > 10; --i) {
+        input.push_back(i);
+    }
+
+    std::cout << "input vector: " << std::endl;
+    for(auto &iter : input) {
+        std::cout << iter << ", ";
+    }
+    std::cout << std::endl;
+
+    size_t times = BubbleSort_min(input);
+
+    std::cout << "after " << times << " sort, vector: " << std::endl;
+    for(auto &iter : input) {
+        std::cout << iter << ", ";
+    }
+    std::cout << std::endl;
+    
+}
+
+void TestMax() {
+    Vector<int> input;
+    for (unsigned i = 1; i < 11; ++i) {
+        input.push_back(i);
+    }
+
+    for (unsigned i = 15; i > 10; --i) {
+        input.push_back(i);
+    }
+
+    std::cout << "input vector: " << std::endl;
+    for(auto &iter : input) {
+        std::cout << iter << ", ";
+    }
+    std::cout << std::endl;
+
+    size_t times = BubbleSort_max(input);
+
+    std::cout << "after " << times << " sort, vector: " << std::endl;
+    for(auto &iter : input) {
+        std::cout << iter << ", ";
+    }
+    std::cout << std::endl;
+    
+}
+int main() {
+    std::cout << "------------------------------" << std::endl; 
+    std::cout << "        min                   " << std::endl;
+    std::cout << "------------------------------" << std::endl; 
+    TestMin();
+
+    std::cout << "------------------------------" << std::endl; 
+    std::cout << "        max                   " << std::endl;
+    std::cout << "------------------------------" << std::endl; 
+    TestMax();
+}
