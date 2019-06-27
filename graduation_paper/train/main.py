d66f5cabfc11a83057603b29b3de9d043668f90d
lidongze0629 <9546726@qq.com>
2019-06-27 11:11:26 +0800

    add train for sssp com-friendster

--- graduation_paper/train/main.py
+++ graduation_paper/train/main.py
@@ -7,6 +7,9 @@ import numpy as np
 
 import sys, time, random
 
+# usa_road 23947348 
+# com-friendster 65608366
+
 if __name__ == '__main__':
     if len(sys.argv) != 6:
         print("Usage: python3 main.py <algorithm> <log_path> <fnum> <total_vertex_num> <L>")
@@ -30,6 +33,8 @@ if __name__ == '__main__':
         t_data, t_label, v_data, v_label = parse_wcc_hashmin_log(fnum, log_path, total_vertex_num, L, "random", 0.98)
     elif algo == "pagerankx":
         t_data, t_label, v_data, v_label = parse_pagerankx_log(fnum, log_path, "random", 0.9)
+    elif algo == "sssp":
+        t_data, t_label, v_data, v_label = parse_sssp_log(fnum, log_path, total_vertex_num, L, "random", 0.9)
     else:
         print("unsupport algo " + algo)
         sys.exit()
@@ -68,7 +73,9 @@ if __name__ == '__main__':
     if algo == "wcc_hashmin":
         plt.suptitle("wcc-hashmin usa-road predict result")
     elif algo == "pagerankx":
-        plt.suptitle("pagerank dbpedia predic result")
+        plt.suptitle("pagerank dbpedia predict result")
+    elif algo == "sssp":
+        plt.suptitle("sssp com-friendster predict result")
 
     plt.plot(list(range(1, predict_num)), figure_results, label='predict')
     plt.plot(list(range(1, predict_num)), figure_labels, label='real')
--- graduation_paper/train/parse.py
+++ graduation_paper/train/parse.py
@@ -1,6 +1,92 @@
 import numpy as np
 import math, random
 
+def parse_sssp_log(fnum, log_path, total_vertex_num, L, mode, proportion):
+    total_datasets = [None] * fnum
+    total_labels = [None] * fnum
+    
+    t_data = [None] * fnum
+    t_label = [None] * fnum
+    v_data = [None] * fnum
+    v_label = [None] * fnum
+
+    for i in range(fnum):
+        v_data[i] = []
+        v_label[i] = []
+
+    log_file = open(log_path, 'r')
+    lines = log_file.readlines()
+    for line in lines:
+        if line.find('TrainLog') != -1:
+            fid, running_time, feature_vector = parse_sssp_log_data(line, total_vertex_num, L)
+            if total_datasets[fid] == None:
+                total_datasets[fid] = []
+                total_labels[fid] = []
+            total_datasets[fid].append(feature_vector)
+            total_labels[fid].append(running_time)
+    log_file.close()
+
+    if mode == "random":
+        for i in range(fnum):
+            total_num = len(total_datasets[i])
+            train_num = math.ceil(total_num * proportion)
+            validate_num = total_num - train_num
+            print("fid-" + str(i) + " total number " + str(total_num) + " train number " + str(train_num) + "validate number " + str(validate_num))
+            random_list = np.unique(np.random.randint(0, total_num, validate_num))
+            random_list_reverse = random_list[::-1]
+            for j in random_list_reverse:
+                v_data[i].append(total_datasets[i][j])
+                v_label[i].append(total_labels[i][j])
+                del total_datasets[i][j]
+                del total_labels[i][j]
+            t_data[i] = total_datasets[i]
+            t_label[i] = total_labels[i]
+    return t_data, t_label, v_data, v_label
+
+def parse_sssp_log_data(line, total_vertex_num, L):
+    interval = math.ceil(total_vertex_num / L);
+    message_embedding = [0] * (interval + 1)
+    
+    data = line[line.find('TrainLog'):]
+    data_array = data.split(';')
+    fid = int(data_array[0][14:])
+    running_time = float(data_array[1]) * 1000
+    ivnum = int(data_array[2])
+    ovnum = int(data_array[3])
+    tvnum = int(data_array[4])
+    edge_num = int(data_array[5])
+    received_msg_num = int(data_array[6])
+    heap_size = int(data_array[7])
+    available_msg_num = int(data_array[8])
+
+    if int(available_msg_num) > 0:
+        update_node = data_array[9]
+        update_node_list = update_node.split(',')
+        del update_node_list[len(update_node_list) - 1]
+        for item in update_node_list:
+            if item == '':
+                continue
+            # print("s: " + item)
+            belong_interval = math.floor(int(item) / L)
+            if belong_interval > interval:
+                continue
+            # print(str(interval) + ", " + str(belong_interval)+ ", " + str(item))
+            message_embedding[belong_interval] = message_embedding[belong_interval] + 1
+
+    # construct feature vector
+    feature_vector = []
+    #feature_vector.append(ivnum)
+    #feature_vector.append(ovnum)
+    #feature_vector.append(tvnum)
+    #feature_vector.append(edge_num)
+    feature_vector.append(received_msg_num)
+    feature_vector.append(heap_size)
+    feature_vector.append(available_msg_num)
+    for item in message_embedding:
+        feature_vector.append(item)
+
+    return fid, running_time, feature_vector
+
 def parse_pagerankx_log(fnum, log_path, mode, proportion):
     total_datasets = [None] * fnum
     total_labels = [None] * fnum
