f17fb515cb74e1b264d6850f11a8a24396dc4260
lidongze0629 <9546726@qq.com>
2019-08-04 21:04:17 +0800

    update metric.py for add MSRE

--- graduation_paper/train/metric.py
+++ graduation_paper/train/metric.py
@@ -5,15 +5,25 @@ def printErrorMetrics(result, labels):
         print("size error when printError")
 
     error = []
+    relative_error = []
+
     absError = []
     squaredError = []
+    relativeError = []
+
     for i in range(len(labels)):
         error.append(float(labels[i]) - float(result[i]))
+        relative_error.append((float(labels[i]) - float(result[i])) / float(labels[i]))
 
     for val in error:
         squaredError.append(val * val)
         absError.append(abs(val))
 
+    for val in relative_error:
+        relativeError.append(val * val)
+
     print("MSE = " + str(sum(squaredError) / len(squaredError)))
     print("MAE = " + str(sum(absError) / len(absError)))
     print("RMSE = " + str(math.sqrt(sum(squaredError) / len(squaredError)))) 
+    print("MSRE = " + str(sum(relativeError) / len(relativeError)))
+
