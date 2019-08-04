d1c3555db03ef56400fa44be5ff196d9d333df20
lidongze0629 <9546726@qq.com>
2019-08-04 20:46:47 +0800

    update main.py

--- graduation_paper/worker_window/main.py
+++ graduation_paper/worker_window/main.py
@@ -4,6 +4,8 @@ import sys
 import matplotlib.pyplot as plt
 
 # com-friendster 65608366
+# livejournal 4847572
+# usa_road 23947348
 
 if __name__ == '__main__':
     logfile = sys.argv[1]
@@ -14,7 +16,7 @@ if __name__ == '__main__':
     total_windows = []
     for i in range(fnum):
         worker_windows.append([])
-    for i in range(100):
+    for i in range(2000):
         total_windows.append(0)
 
     max_step = 0
@@ -32,17 +34,22 @@ if __name__ == '__main__':
                total_windows[step] = total_windows[step] + num
                if step > max_step:
                    max_step = step
+     
+    if max_step > 100:
+        max_step = 100
     with open(logfile + '.output', 'w') as f:
         f.write(str(total_windows[:max_step + 1]))
 
     # print(len(list(range(1, max_step + 1))))
     # print(len(total_windows[:max_step + 1]))
+    print(total_windows[:max_step + 1])
     for i in range(step + 1):
         # here you should seted by graph
-        total_windows[i] = total_windows[i] / 65608366
+        total_windows[i] = total_windows[i] / 23947348
     plt.plot(list(range(1, max_step + 2)), total_windows[:max_step + 1], linestyle='dotted', color='black', marker='*', ms=6)
     plt.xlabel('superstep')
     plt.ylabel('window size')
+    #plt.ylim(0.0, 1.2)
     plt.legend()
     plt.savefig(logfile + '.png')
     plt.show()
