c727711bfdabb291e0997565ebe648ed4db1a0a4
lidongze0629 <9546726@qq.com>
2019-08-12 00:29:03 +0800

    add pagerank

--- graduation_paper/train/nn.py
+++ graduation_paper/train/nn.py
@@ -6,10 +6,10 @@ from metric import printErrorMetrics
 
 import numpy as np
 from sklearn.model_selection import KFold
-from sklearn.ensemble import RandomForestRegressor
+from sklearn.neural_network import MLPRegressor
 
-def rf(datasets, labels, fnum):
-    print('--- random forest ---')
+def nn(datasets, labels, fnum):
+    print('--- neural network ---')
     TMSE = 0; TMAE = 0; TRMSE = 0; TMSRE = 0
     sample_results = []
     sample_labels = []
@@ -22,7 +22,7 @@ def rf(datasets, labels, fnum):
         """ KFold """
         index = 1
         MSE = 0; MAE = 0; RMSE = 0; MSRE = 0
-        regr = RandomForestRegressor(n_estimators=100)
+        clf = MLPRegressor()
         kf = KFold(n_splits=10, shuffle=True, random_state=np.random.randint(0,100))
 
         for train_index, test_index in kf.split(datasets_np):
@@ -31,11 +31,11 @@ def rf(datasets, labels, fnum):
             x_train, x_test = datasets_np[train_index], datasets_np[test_index]
             y_train, y_test = labels_np[train_index], labels_np[test_index]
             start = time.process_time()
-            regr.fit(x_train, y_train)
+            clf.fit(x_train, y_train)
             end = time.process_time()
             times.append(end - start)
             # print(regr.feature_importances_)
-            result = regr.predict(x_test)
+            result = clf.predict(x_test)
             a, b, c, d = printErrorMetrics(result, y_test)
             sample_labels.extend(y_test)
             sample_results.extend(result)
--- graduation_paper/train/rr.py
+++ graduation_paper/train/rr.py
@@ -6,10 +6,10 @@ from metric import printErrorMetrics
 
 import numpy as np
 from sklearn.model_selection import KFold
-from sklearn.ensemble import RandomForestRegressor
+from sklearn.linear_model import Ridge
 
-def rf(datasets, labels, fnum):
-    print('--- random forest ---')
+def rr(datasets, labels, fnum):
+    print('--- ridge regression ---')
     TMSE = 0; TMAE = 0; TRMSE = 0; TMSRE = 0
     sample_results = []
     sample_labels = []
@@ -22,7 +22,7 @@ def rf(datasets, labels, fnum):
         """ KFold """
         index = 1
         MSE = 0; MAE = 0; RMSE = 0; MSRE = 0
-        regr = RandomForestRegressor(n_estimators=100)
+        rlf = Ridge(alpha=1.0)
         kf = KFold(n_splits=10, shuffle=True, random_state=np.random.randint(0,100))
 
         for train_index, test_index in kf.split(datasets_np):
@@ -31,11 +31,11 @@ def rf(datasets, labels, fnum):
             x_train, x_test = datasets_np[train_index], datasets_np[test_index]
             y_train, y_test = labels_np[train_index], labels_np[test_index]
             start = time.process_time()
-            regr.fit(x_train, y_train)
+            rlf.fit(x_train, y_train)
             end = time.process_time()
             times.append(end - start)
             # print(regr.feature_importances_)
-            result = regr.predict(x_test)
+            result = rlf.predict(x_test)
             a, b, c, d = printErrorMetrics(result, y_test)
             sample_labels.extend(y_test)
             sample_results.extend(result)
